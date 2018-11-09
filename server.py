5557c49d4497a4bbabc9448236c7cac1119b2a31
Juan Carlos Quirino <juanquirinoc@gmail.com>
2018-11-09 12:25:50 -0600

    Added view to GET /vote

--- server.py
+++ server.py
@@ -19,12 +19,17 @@ def root():
         'text':text,
         'created_at':tweet.created_at
     }
-    return f'''<!DOCTYPE html>\n<html>\n\n<head>\n    <meta charset="utf-8" />\n    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n    <title>Page Title</title>\n    <meta name="viewport" content="width=device-width, initial-scale=1">\n    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="\n      crossorigin="anonymous"></script>\n    <script src="{{url_for('static', filename='main.js')}}"></script>\n</head>\n \n<body>\n    <center>\n        <h1>Fake or not</h1>\n        <p      id="tweet">{data['text']}</p>\n        <button id="fake">Fake</button>\n        <button id="real">Real</button>\n    </center>\n\n</body>\n\n</html>'''
-
+    return jsonify(data)
+ 
 @app.route('/', methods=['POST'])
 def root_post():
     print(request.form)
     return "OKAY" 
+
+@app.route('/vote',methods=['GET'])
+def vote():
+    with open('index.html') as f:
+        return f.read()
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=os.environ['PORT'],debug=True)
