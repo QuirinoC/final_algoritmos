37bc7b2b65f0d5587c11ea1572e7bfa5906b7bda
Juan Carlos Quirino <juanquirinoc@gmail.com>
2018-11-23 00:41:05 -0600

    Modified clean_csv

--- clean_csv.py
+++ clean_csv.py
@@ -5,5 +5,6 @@ import csv
 df = pd.read_csv('l.csv',index_col=0)
 
 df['text'] = df['text'].apply(replace_string)
-print(df)
+df = df.set_index((df.index.astype(int)))
+#df['id']   = df['id'].to_numeric()
 df.to_csv('tweets.csv',quoting=csv.QUOTE_NONNUMERIC)
